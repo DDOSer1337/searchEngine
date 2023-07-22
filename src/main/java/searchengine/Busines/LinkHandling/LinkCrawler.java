2f40776016666ebc91aed27d617984a7a35f140a
DDOSer1337 <59763808+DDOSer1337@users.noreply.github.com>
2023-07-22 21:12:26 +0800

    Добавил способ взаимодействия с БД при помощи репозиторий

--- src/main/java/searchengine/Busines/LinkHandling/LinkCrawler.java
+++ src/main/java/searchengine/Busines/LinkHandling/LinkCrawler.java
@@ -1,5 +1,6 @@
 package searchengine.Busines.LinkHandling;
 
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -7,28 +8,26 @@ import org.jsoup.nodes.Element;
 import searchengine.model.Index;
 import searchengine.model.Lemma;
 import searchengine.model.Page;
+import searchengine.model.Repository.IndexRepository;
+import searchengine.model.Repository.LemmaRepository;
+import searchengine.model.Repository.PageRepository;
+import searchengine.model.Repository.SiteRepository;
 import searchengine.model.Site;
-
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.RecursiveAction;
 
+@RequiredArgsConstructor
 public class LinkCrawler extends RecursiveAction {
 
     private final String domain, currentLink;
     private final Set<String> verifiedLinks;
     private final Site site;
-    private final DBConnector dbConnector = new DBConnector();
-
-
-    public LinkCrawler(String domain, String currentLink, Set<String> verifiedLinks, Site site) {
-        this.domain = domain;
-        this.currentLink = currentLink;
-        this.verifiedLinks = verifiedLinks;
-        this.site = site;
-
-    }
+    private final SiteRepository siteRepository;
+    private final PageRepository pageRepository;
+    private final LemmaRepository lemmaRepository;
+    private final IndexRepository indexRepository;
+    private DBConnector dbConnector = new DBConnector();
 
     @SneakyThrows
     @Override
@@ -56,45 +55,20 @@ public class LinkCrawler extends RecursiveAction {
     private void recursiveActionFork(String newLink, Connection connection) {
         if (isCorrectUrl(newLink)) {
             Page page = new Page(newLink, connection.get(), domain, site, connection.execute().statusCode());
-            pageUploader(page);
+            pageRepository.save(page);
             LemmaSearch lemmaSearch = new LemmaSearch(site, connection.get().text());
             List<Lemma> lemmaList = lemmaSearch.invoke();
             IndexCreator(page, lemmaList);
-            LinkCrawler recursiveTask = new LinkCrawler(newLink, domain, verifiedLinks, site);
+            LinkCrawler recursiveTask = new LinkCrawler(newLink, domain, verifiedLinks, site, siteRepository, pageRepository, lemmaRepository, indexRepository);
             recursiveTask.fork();
         }
     }
 
-    private void pageUploader(Page page) throws SQLException {
-        dbConnector.getConnection().createStatement().executeQuery("INSERT INTO `skillbox`.`pages`\n" +
-                "(`id`,\n" +
-                "`code`,\n" +
-                "`content`,\n" +
-                "`path`,\n" +
-                "`sites_id`)\n" +
-                "VALUES\n" +
-                "("+ page.getId()+",\n" +
-                page.getCode()+",\n" +
-                page.getContent()+",\n" +
-                page.getPath()+",\n" +
-                page.getSiteId()+");");
-        System.out.println("pageUploader");
-    }
-    private void IndexCreator(Page page, List<Lemma> lemmaList) throws SQLException {
-        int counterForUploadingToDatabase = 0;
-        StringBuilder builder = new StringBuilder();
+    private void IndexCreator(Page page, List<Lemma> lemmaList){
         for (Lemma lemma : lemmaList) {
             Index index = new Index(page, lemma);
-            String indexData = " (" + index.getId() + ", " + index.getRank() + index.getLemmaId() + ", " + index.getPageId() + ")";
-            builder.append(builder.length() == 0 ? "" : ",").append(indexData);
-            counterForUploadingToDatabase++;
-            if (counterForUploadingToDatabase == 50){
-                indexUploader(builder.toString());
-                builder = new StringBuilder();
-            }
+            indexRepository.save(index);
         }
-        indexUploader(builder.toString());
-
     }
 
     private boolean isCorrectUrl(String newLink) {
@@ -103,13 +77,5 @@ public class LinkCrawler extends RecursiveAction {
                 && !newLink.contains("?")
                 && newLink.contains(domain)
                 && !newLink.contains("#");
-    }
-    private void indexUploader(String content) throws SQLException {
-        String sql = "INSERT INTO `skillbox`.`indices`\n" +
-                "(`id`,\n" +
-                "`rank`,\n" +
-                "`lemmas_id`,\n" +
-                "`pages_id`) VALUES" + content + "ON DUPLICATE KEY UPDATE `rank` = `rank` + 1 ";
-        dbConnector.getConnection().createStatement().executeQuery(sql);
     }
 }
