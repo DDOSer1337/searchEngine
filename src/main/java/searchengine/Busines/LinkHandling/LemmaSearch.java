22183d0a9c802e7870461f8012d55529710e3e09
DDOSer1337 <59763808+DDOSer1337@users.noreply.github.com>
2023-07-23 23:37:06 +0800

    Реализовал класс лемматизации  и переменовал метод на подходящее название
    была сделана лемматизация ру и енг слов
    метод getLemma стал getLemmas

--- src/main/java/searchengine/Busines/LinkHandling/LemmaSearch.java
+++ src/main/java/searchengine/Busines/LinkHandling/LemmaSearch.java
@@ -1,29 +1,77 @@
 package searchengine.Busines.LinkHandling;
 
+import lombok.SneakyThrows;
+import org.apache.lucene.morphology.LuceneMorphology;
+import org.apache.lucene.morphology.english.EnglishLuceneMorphology;
+import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
 import searchengine.model.Lemma;
 import searchengine.model.Site;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
-import java.util.concurrent.RecursiveTask;
 
-public class LemmaSearch extends RecursiveTask<List<Lemma>> {
-    private Site link;
-    private String text;
-    private String sql = "INSERT INTO `skillbox`.`lemmas`(`id`," +
-            "`frequency`," +
-            "`lemma`," +
-            "`sites_id`) VALUES" +"content"+
-            "ON DUPLICATE KEY UPDATE `frequency` = `frequency` + 1 ";
+public class LemmaSearch {
+    private final Site site;
+    private final String text;
 
-    public LemmaSearch(Site link, String text) {
-        this.link = link;
+    public LemmaSearch(Site site, String text) {
+        this.site = site;
         this.text = text;
     }
 
+    @SneakyThrows
+    public List<Lemma> getLemmas() {
+        List<Lemma> lemmaList = new ArrayList<>();
+        List<String> textList = new ArrayList<>();
+        Collections.addAll(textList, text.split(" "));
+        for (String word : textList) {
+            String word2LowerCase = word.toLowerCase(Locale.ROOT).trim();
+            Lemma lemma = createLemma(word2LowerCase);
+            if (lemma != null) {
+                lemmaList.add(lemma);
+            }
+        }
+        return lemmaList;
+    }
 
-    @Override
-    protected List<Lemma> compute() {
-        return null;
+    private boolean isRussian(String word) {
+        return (word.matches("[а-яА-Я]+"));
+    }
+
+    private boolean isEnglish(String word) {
+        return (word.matches("[a-zA-Z]+"));
+    }
+
+    private boolean isNotPartOfSpeech(String word) {
+        return !(word.endsWith("Н") || word.endsWith("МЕЖД") || word.endsWith("СОЮЗ") ||
+                word.endsWith("PREP") || word.endsWith("ADJECTIVE") || word.endsWith("CONJ") ||
+                word.endsWith("ARTICLE") || word.endsWith("ADVERB"));
+    }
+
+    private Lemma createLemma(String word2LowerCase) throws IOException {
+        Lemma lemma = null;
+        LuceneMorphology luceneMorphology = null;
+        if (isRussian(word2LowerCase)) {
+            luceneMorphology = new RussianLuceneMorphology();
+        }
+        if (isEnglish(word2LowerCase)) {
+            luceneMorphology = new EnglishLuceneMorphology();
+        }
+        if (luceneMorphology != null) {
+            List<String> getMorphInfo = luceneMorphology.getMorphInfo(word2LowerCase);
+            String morphInfo = getMorphInfo.get(0);
+            if (isNotPartOfSpeech(morphInfo)) {
+                List<String> we = luceneMorphology.getNormalForms(word2LowerCase);
+                lemma = new Lemma();
+                lemma.setSiteId(site);
+                lemma.setLemma(we.get(0));
+                lemma.setFrequency(1);
+            }
+        }
+        return lemma;
     }
 }
