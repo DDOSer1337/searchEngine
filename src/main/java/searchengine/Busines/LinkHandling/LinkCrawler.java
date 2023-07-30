61e9319cc7ba3398d79f219fa3f369c9c45bbf99
DDOSer1337 <59763808+DDOSer1337@users.noreply.github.com>
2023-07-30 10:34:09 +0500

    Добавил обработку ошибки при подключении к сайту

--- src/main/java/searchengine/Busines/LinkHandling/LinkCrawler.java
+++ src/main/java/searchengine/Busines/LinkHandling/LinkCrawler.java
@@ -4,7 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import searchengine.Busines.LinkHandling.Creator.IndexCreator;
+import searchengine.Busines.LinkHandling.Creator.LemmaCreator;
+import searchengine.Busines.LinkHandling.Creator.PageCreator;
+import searchengine.model.Enum.siteStatus;
 import searchengine.model.Index;
 import searchengine.model.Lemma;
 import searchengine.model.Page;
@@ -13,6 +18,10 @@ import searchengine.model.Repository.LemmaRepository;
 import searchengine.model.Repository.PageRepository;
 import searchengine.model.Repository.SiteRepository;
 import searchengine.model.Site;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.RecursiveAction;
@@ -27,55 +36,68 @@ public class LinkCrawler extends RecursiveAction {
     private final PageRepository pageRepository;
     private final LemmaRepository lemmaRepository;
     private final IndexRepository indexRepository;
-    private DBConnector dbConnector = new DBConnector();
 
     @SneakyThrows
     @Override
     protected void compute() {
-        Thread.sleep(200);
-        if (!verifiedLinks.contains(currentLink)) {
+        Thread.sleep(500);
+        if (!(verifiedLinks.contains(currentLink) && LinkParser.isNeedToStopTheParser())) {
             linkChecking();
         }
     }
 
-    @SneakyThrows
     private void linkChecking() {
         Connection connection = Jsoup.connect(currentLink)
                 .userAgent("Mozilla/5.0 (Windows; U; WindowsNT " +
                         "5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
                 .referrer("http://www.google.com");
-        for (Element link : connection.get().select("a[href]")) {
-            String newLink = link.attr("abs:href");
-            recursiveActionFork(newLink, connection);
+        try {
+            List<Element> links = connection.get().select("a[href]");
+            Document document = connection.get();
+            System.out.println("\nподключено :" + currentLink + "\n");
+            for (Element link : links) {
+                String newLink = link.attr("abs:href");
+                recursiveActionFork(newLink, document);
+            }
+            verifiedLinks.add(currentLink);
+        } catch (IOException e) {
+            exceptionSite(site);
+            e.printStackTrace();
         }
-        verifiedLinks.add(currentLink);
     }
 
-    @SneakyThrows
-    private void recursiveActionFork(String newLink, Connection connection) {
-        if (isCorrectUrl(newLink)) {
-            Page page = new Page(newLink, connection.get(), domain, site, connection.execute().statusCode());
-            pageRepository.save(page);
-            LemmaSearch lemmaSearch = new LemmaSearch(site, connection.get().text());
-            List<Lemma> lemmaList = lemmaSearch.getLemmas();
-            IndexCreator(page, lemmaList);
+    private void recursiveActionFork(String newLink, Document document) {
+        if (isCorrectUrl(newLink) && !verifiedLinks.contains(newLink)) {
+            PageCreator pageCreator = new PageCreator(newLink,siteRepository,lemmaRepository,pageRepository);
+            Page page = pageCreator.getPage();
+            List<Lemma> lemmaList =  new LemmaCreator(site, document.text(), lemmaRepository).getLemmas();
+            for (Lemma lemma : lemmaList){
+                IndexCreator indexCreator = new IndexCreator(page,lemma,indexRepository);
+                indexCreator.indexCreate();
+            }
             LinkCrawler recursiveTask = new LinkCrawler(newLink, domain, verifiedLinks, site, siteRepository, pageRepository, lemmaRepository, indexRepository);
             recursiveTask.fork();
         }
     }
 
-    private void IndexCreator(Page page, List<Lemma> lemmaList){
-        for (Lemma lemma : lemmaList) {
-            Index index = new Index(page, lemma);
-            indexRepository.save(index);
-        }
-    }
-
     private boolean isCorrectUrl(String newLink) {
-        return newLink.contains("http")
+        return newLink != null
+                && newLink.contains("http")
                 && !newLink.contains("pdf")
                 && !newLink.contains("?")
                 && newLink.contains(domain)
                 && !newLink.contains("#");
+    }
+    private void exceptionSite(Site site){
+        String sql = "UPDATE `skillbox`.`sites`\n" +
+                "SET\n" +
+                "`site_status` = '"+siteStatus.FAILED+"',\n" +
+                "`last_error` = 'Ошибка индексации: главная страница сайта не доступна'\n" +
+                "WHERE `url` = '" + site.getUrl()+"'";
+        try {
+            new DBConnector().getStatement().executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
