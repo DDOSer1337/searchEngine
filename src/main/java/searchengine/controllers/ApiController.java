d4b91a12621b6a263c9ccd1f569f35d9809754e9
DDOSer1337 <59763808+DDOSer1337@users.noreply.github.com>
2023-07-30 10:39:06 +0500

    Добавил методы
    метод search еще не реализован

--- src/main/java/searchengine/controllers/ApiController.java
+++ src/main/java/searchengine/controllers/ApiController.java
@@ -1,11 +1,16 @@
 package searchengine.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import searchengine.Busines.LinkHandling.Creator.PageCreator;
+import searchengine.Busines.LinkHandling.LinkParser;
 import searchengine.dto.statistics.StatisticsResponse;
+import searchengine.model.Page;
+import searchengine.model.Repository.LemmaRepository;
+import searchengine.model.Repository.PageRepository;
+import searchengine.model.Repository.SiteRepository;
 import searchengine.services.StatisticsServiceImpl;
 
 import java.sql.SQLException;
@@ -14,8 +19,13 @@ import java.sql.SQLException;
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-
     private StatisticsServiceImpl statisticsService;
+    @Autowired
+    private PageRepository pageRepository;
+    @Autowired
+    private SiteRepository siteRepository;
+    @Autowired
+    private LemmaRepository lemmaRepository;
 
     public ApiController(StatisticsServiceImpl statisticsService) {
         this.statisticsService = statisticsService;
@@ -23,7 +33,7 @@ public class ApiController {
 
     @GetMapping("/statistics")
     public ResponseEntity<StatisticsResponse> statistics() {
-            return ResponseEntity.ok(statisticsService.getStatistics());
+        return ResponseEntity.ok(statisticsService.getStatistics());
     }
 
     @GetMapping("/startIndexing")
@@ -31,8 +41,27 @@ public class ApiController {
         try {
             statisticsService.startParse();
         } catch (SQLException e) {
-             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(statisticsService.getStatistics());
         }
         return ResponseEntity.status(HttpStatus.OK).body(statisticsService.getStatistics());
     }
+
+    @GetMapping("/stopIndexing")
+    public ResponseEntity<?> stopIndexing() {
+        return LinkParser.stopParse();
+    }
+
+    @PostMapping("/indexPage")
+    public ResponseEntity<?> indexPage(@RequestParam("url") String url) {
+        Page page = new PageCreator(url, siteRepository, lemmaRepository, pageRepository).getPage();
+        return PageCreator.getPageResponse(page);
+
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<?> search(@RequestParam("url") String url) {
+        return null;
+    }
+
 }
